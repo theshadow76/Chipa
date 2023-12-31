cb985c1bb286266c3909e13679c2897ca78b6062
Vigo Walker <vigopaul05@gmail.com>
2023-12-31 18:26:15 -0300

    first commit

---
+++ svelte/src/components/SidePanel.svelte
@@ -0,0 +1,53 @@
+<script>
+
+</script>
+
+<style>
+    .SidePanel {
+        background-color: #212121;
+    }
+    .SidePanel-ListObj ul {
+        list-style-type: none;
+    }
+    .SidePanel-ListObj img {
+        width: 30px;
+        height: 30px;
+    }
+    .SidePanel {
+        width: 10%;
+        height: 100%;
+    }
+</style>
+
+<div class="SidePanel">
+    <h3 class="Title">Chipa: Crypto Wallet</h3>
+    <div class="SidePanel-ListObj">
+        <!--List of objects-->
+        <ul>
+            <li>
+                <img src="https://cdn4.iconfinder.com/data/icons/business-finance-vol-12-2/512/10-512.png" alt="planet_icon">
+                <h3>Dashboard</h3>
+            </li>
+            <li>
+                <img src="https://cdn4.iconfinder.com/data/icons/business-finance-vol-12-2/512/10-512.png" alt="planet_icon">
+                <h3>Wallet</h3>
+            </li>
+            <li>
+                <img src="https://cdn4.iconfinder.com/data/icons/business-finance-vol-12-2/512/10-512.png" alt="planet_icon">
+                <h3>Trade</h3>
+            </li>
+            <li>
+                <img src="https://cdn4.iconfinder.com/data/icons/business-finance-vol-12-2/512/10-512.png" alt="planet_icon">
+                <h3>Market</h3>
+            </li>
+            <li>
+                <img src="https://cdn4.iconfinder.com/data/icons/business-finance-vol-12-2/512/10-512.png" alt="planet_icon">
+                <h3>Notification</h3>
+            </li>
+            <li>
+                <img src="https://cdn4.iconfinder.com/data/icons/business-finance-vol-12-2/512/10-512.png" alt="planet_icon">
+                <h3>Setings</h3>
+            </li>
+        </ul>
+    </div>
+</div>
